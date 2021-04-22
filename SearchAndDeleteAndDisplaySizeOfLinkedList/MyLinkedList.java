240e4d01a9767b4fc76ffa59b252fcc2b0906441
rushitayade71 <rushitayade71@gmail.com>
2021-04-22 18:44:03 +0530

    Added: Search and delete 40 from LinkedList and display size of LinkedList

---
+++ SearchAndDeleteAndDisplaySizeOfLinkedList/MyLinkedList.java
@@ -0,0 +1,121 @@
+package linkedList;
+
+public class MyLinkedList<K> {
+
+	 public INode head;
+	    public INode tail;
+
+	    public MyLinkedList() {
+	        this.head = null;
+	        this.tail = null;
+	    }
+
+	    public void add(INode myNewNode) {
+	        if(this.tail == null) {
+	            this.tail = myNewNode;
+	        }if (this.head == null){
+	            this.head = myNewNode;
+	        }else {
+	            INode tempNode = this.head;
+	            this.head = myNewNode;
+	            this.head.setNext(tempNode);
+	        }
+	    }
+
+	    public void printNodes() {
+	        StringBuffer nodes = new StringBuffer();
+	        INode tempNode = head;
+	        while(tempNode.getNext() != null) {
+	            nodes.append(tempNode.getKey());
+	            if(tempNode != this.tail){
+	                nodes.append("->");
+	            }
+	            tempNode = tempNode.getNext();
+	        }
+	        nodes.append(tempNode.getKey());
+	        System.out.println(nodes);
+	    }
+
+
+	    public void append(INode myNewNode) {
+	        if(this.head == null) {
+	            this.head = myNewNode;
+	        }
+	        if(this.tail == null){
+	            this.tail = myNewNode;
+	        }else {
+	            this.tail.setNext(myNewNode);
+	            this.tail = myNewNode;
+
+	        }
+	    }
+
+	    public void insert(INode myNode,INode myNewNode) {
+	        INode tempNode = myNode.getNext();
+	        myNode.setNext(myNewNode);
+	        myNewNode.setNext(tempNode);
+	    }
+
+	    public INode pop() {
+	        INode tempNode = this.head;
+	        this.head=head.getNext();
+	        return tempNode;
+	    }
+	    public INode popLast() {
+	        INode tempNode = head;
+	        while(!tempNode.getNext().equals(tail)){
+	            tempNode = tempNode.getNext();
+	        }
+	        
+	        this.tail = tempNode;
+	        tempNode.setNext(null);
+	        return tempNode;
+	    }
+	    
+	    public boolean search(K key) {
+	        INode tempNode = head;
+	        while (tempNode != null && tempNode.getNext() != null) {
+	            if(tempNode.getNext().getKey().equals(key)){
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	    
+	    public void searchAndInsert(int num, INode myNewNode){
+	        INode tempNode = head;
+	        while (tempNode.getNext() != null){
+	            if(tempNode.getKey().equals(num)) {
+	                INode temp = tempNode.getNext();
+	                tempNode.setNext(myNewNode);
+	                myNewNode.setNext(temp);
+	            }
+	            tempNode = tempNode.getNext();
+	        }
+	    }
+	    
+	    public void searchAndDelete(int num) {
+	        INode tempNode = head;
+	        while (tempNode.getNext() != null) {
+	            if (tempNode.getNext().getKey().equals(num)) {
+	                INode temp = tempNode.getNext().getNext();
+	                tempNode.setNext(temp);
+	            }
+	            tempNode = tempNode.getNext();
+	        }
+	    }
+
+	    public int size()
+	    {
+	        INode temp = head;
+	        int count = 0;
+	        while (temp != null)
+	        {
+	            count++;
+	            temp = temp.getNext();
+	        }
+	        return count;
+	    }
+
+
+	}
---
+++ SearchAndDeleteAndDisplaySizeOfLinkedList/MyNodeTest.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MyNodeTest {
+
+    @Test
+    public void given3Numbers_whenAdddedToLinkedList_ShouldAppearOnTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+}
