6deea8d302f16c3dd061bced0812874b8949bbc6
rushitayade71 <rushitayade71@gmail.com>
2021-04-22 17:00:26 +0530

    Added: Delete last element in Linked List

---
+++ DeleteLastElementInLinkedList/MyLinkedList.java
@@ -0,0 +1,72 @@
+package linkedList;
+
+public class MyLinkedList {
+
+	 public INode head;
+	    public INode tail;
+
+	    public MyLinkedList() {
+	        this.head = null;
+	        this.tail = null;
+	    }
+
+	    public void add(INode myNode) {
+	        if(this.head == null && this.tail == null){
+	            this.head = myNode;
+	            this.tail = myNode;
+	        }else{
+	            INode temp = this.head;
+	            this.head = myNode;
+	            this.head.setNext(temp);
+	        }
+	    }
+
+	    public void printNodes() {
+	        StringBuffer nodes = new StringBuffer();
+	        INode tempNode = head;
+	        while(tempNode.getNext() != null) {
+	            nodes.append(tempNode.getKey());
+	            if(tempNode != this.tail){
+	                nodes.append("->");
+	            }
+	            tempNode = tempNode.getNext();
+	        }
+	        nodes.append(tempNode.getKey());
+	        System.out.println(nodes);
+	    }
+
+	    public void append(INode myNode) {
+	        if(this.head == null) {
+	            this.head = myNode;
+	        }
+	        if(this.tail == null){
+	            this.tail = myNode;
+	        }else {
+	            this.tail.setNext(myNode);
+	            this.tail = myNode;
+
+	        }
+	    }
+
+	    public void insert(INode myNode,INode newNode) {
+	        INode tempNode = myNode.getNext();
+	        myNode.setNext(newNode);
+	        newNode.setNext(tempNode);
+	    }
+
+	    public INode pop() {
+	        INode tempNode = this.head;
+	        this.head=head.getNext();
+	        return tempNode;
+	    }
+	    public INode popLast() {
+	        INode tempNode = head;
+	        while(!tempNode.getNext().equals(tail)){
+	            tempNode = tempNode.getNext();
+	        }
+	        
+	        this.tail = tempNode;
+	        tempNode.setNext(null);
+	        return tempNode;
+	    }
+	}
